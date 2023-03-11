07e329b27dab00dc8120a2d4ddc0e900894c1adf
Fabiano Matos <fabiano.matoz@gmail.com>
2023-03-11 17:36:01 -0300

    fix install.sh error

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 # Created: Thursday, 2023/03/09 - 21:34:21
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Thursday, 2023/03/09 - 21:34:57
+# Last Modified: Saturday, 2023/03/11 - 17:41:20
 # Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.1.3
+# Version: 0.0.4.8
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  >
@@ -19,16 +19,16 @@
 
 github=https://github.com
 username=fbnmtz
-repo=porlybarctl
+repo=polybarctl
 
 if [ ! -d ~/bin/xSHELL ] || [ ! -L ~/bin/xSHELL ]; then
-    curl -sSL https://raw.githubusercontent.com/fbnmtz/shell/master/install.sh | bash 
+    curl -sSL https://raw.githubusercontent.com/fbnmtz/xSHELL/master/install.sh | bash 
 fi
 
 if [ ! -d ~/bin/xSHELL/packages/${repo} ] || [ ! -L ~/bin/xSHELL/packages/${repo} ]; then
 
     # clone this repo to your '/home'
-    git clone "${github}/${username}/${repo}" ~/bin/xSHELL/packages/${repo} && \
+    git clone "${github}/${username}/${repo}" ~/bin/xSHELL/packages/${repo}
     
     # Add scripts to your system $PATH
     if ! grep "\$PATH:\$HOME/bin/xSHELL/packages/${repo}" ~/.profile  1> /dev/null  ; then
